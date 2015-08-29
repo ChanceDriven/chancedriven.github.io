2234345dd321e4786138d8d0848d333abec3d0d4
ChanceDriven <robert.m.rodriguez@emerson.com>
2015-08-29 11:39:02 -0500

    reset

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DarkThemedSite")]
+[assembly: AssemblyTitle("RouteTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DarkThemedSite")]
+[assembly: AssemblyProduct("RouteTest")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1ed62db2-1882-4443-bf02-0894fe191277")]
+[assembly: Guid("1c8439de-f5dd-4622-9419-4484efc1e444")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ appModule.js
@@ -0,0 +1,54 @@
+﻿"use strict";
+
+var mod = angular.module('app', ['ngRoute']);
+
+/* controller */
+
+angular.module('app').controller('appController', ['$scope', '$location', function ($scope, $location) {
+    $scope.showFirst = function () {
+        $location.url('/first');
+    };
+
+    $scope.showSecond = function () {
+        $location.url('/second');
+    };
+
+    $scope.showThird = function () {
+        $location.url('/third');
+    };
+}]);
+
+/* routes  */
+
+angular.module('app').config(['$routeProvider', function ($routeProvider) {
+
+    var routes = [
+        {
+            url: '/first',
+            config: {
+                templateUrl: 'first.html',
+                controller: 'appController'
+            }
+        },
+        {
+            url: '/second',
+            config: {
+                templateUrl: 'second.html',
+                controller: 'appController'
+            }
+        },
+        {
+            url: '/third',
+            config: {
+                templateUrl: 'third.html',
+                controller: 'appController'
+            }
+        }
+    ];
+
+    routes.forEach(function (route) {
+        $routeProvider.when(route.url, route.config)
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/first', controller: 'appController' });
+}]);
